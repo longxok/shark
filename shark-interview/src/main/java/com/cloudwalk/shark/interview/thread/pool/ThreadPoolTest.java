76b966fbb49a2acc466704ca1e44757f0012270c
yuanhao <yuanhao@cloudwalk.cn>
2019-07-05 18:05:42 +0800

    命名调整

---
+++ shark-interview/src/main/java/com/cloudwalk/shark/interview/thread/pool/ThreadPoolTest.java
@@ -0,0 +1,49 @@
+package com.cloudwalk.shark.interview.thread.pool;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author: yuanhao
+ * @version: v1.0
+ * @description: com.cloudwalk.shark.interview.thread.pool
+ * @date:2019/7/5
+ */
+public class ThreadPoolTest {
+    private static int i=0;
+    private static Object object = new Object();
+    public static AtomicInteger  threadIndex = new AtomicInteger();
+    public static ThreadPoolExecutor executors = new ThreadPoolExecutor(3, 3, 10, TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(1), new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r);
+            t.setName("Thread"+threadIndex.getAndIncrement());
+            return t;
+        }
+    },new ThreadPoolExecutor.CallerRunsPolicy());
+
+    public static void main(String args[]){
+        for(int i=0;i<10;i++){
+            RunClass  thread = new RunClass();
+            executors.execute(thread);
+        }
+    }
+
+    static class RunClass implements Runnable{
+        @Override
+        public void run() {
+
+               // Thread.sleep(2000);
+                synchronized (object){
+                    i++;
+                }
+                System.out.println("thread name is "+Thread.currentThread().getName()+"&&"+i);
+
+        }
+    }
+
+}
